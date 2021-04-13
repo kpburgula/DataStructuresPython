09ce485d63a5b17b4d8bc3d4df97ee08407c818c
kpburgula <>
2021-04-13 04:40:04 +0000

    added double linked lists and modified linked lists

--- code/double_linked_lists.py
+++ code/double_linked_lists.py
@@ -2,10 +2,12 @@ class Node:
   def __init__(self,val):
     self.data = val
     self.next = None
-
-class Linkedlist:
+    self.prev = None
+  
+class DoublyLinkedlist:
   def __init__(self):
     self.head = None
+    self.prev = None
     self.next = None
     self.tail = None
     self.length = 0
@@ -13,10 +15,12 @@ class Linkedlist:
   def append(self, value):
     new_node = Node(value)
     if self.head is None:
+      new_node.prev = None
       self.head = new_node
       self.tail = self.head
       self.length = self.length + 1
     else:
+      new_node.prev = self.tail
       self.tail.next = new_node
       self.tail = new_node
       self.length = self.length + 1
@@ -26,6 +30,7 @@ class Linkedlist:
   def prepend(self,value):
     new_node = Node(value)
     new_node.next = self.head
+    self.head.prev = new_node
     self.head = new_node
     self.length = self.length + 1
     return self.print_list()
@@ -52,6 +57,8 @@ class Linkedlist:
         if i == index:
           prev_node.next = new_node
           new_node.next = curr_node
+          new_node.prev = prev_node
+          curr_node.prev = new_node
           self.length = self.length + 1
           break
     return self.print_list()
@@ -71,33 +78,21 @@ class Linkedlist:
         if i == index:
           print(prev_node.data,curr_node.data)
           prev_node.next = curr_node.next
+          curr_node.prev = prev_node
           self.length = self.length - 1
           break
       return self.print_list()
 
-  def reverse(self):
-    prev = None
-    self.tail = self.head
-    while (self.head!=None):
-      temp = self.head
-      self.head = self.head.next
-      temp.next = prev
-      prev = temp
-
-    self.head = temp
-
-    return self.print_list()
 
 
-
-l = Linkedlist()
+l = DoublyLinkedlist()
 print(l.append(5))
 l.append(8)
 l.append(19)
-l.prepend(100)
-print(l.prepend(200))
-print(l.insert(1,'inserted'))
+# l.prepend(100)
+# print(l.prepend(200))
+# print(l.insert(1,'inserted'))
+print(l.print_list())
 print(l.remove(3))
-print(l.reverse())
 
 
