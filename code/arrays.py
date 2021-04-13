031abef9d9009047dd2f60aaafc9ad5786a15897
kpburgula <>
2021-04-13 07:19:52 +0000

    Added few useful functions on arrays

--- code/arrays.py
+++ code/arrays.py
@@ -40,5 +40,34 @@ a.push('fourth item')
 # a.pop()
 print(a.delete(2))
 print(a)
+
+# Reverse a string
+
+def reverse_string(sentence):
+  return ''.join(reversed(sentence))
   
+def merged_sorted_array(a,b):
+  if len(a) == 0 or len(b) == 0:
+    return a+b
+
+  else:
+    i=0
+    j=0
+    myarray = []
+    while len(a)>i and len(b)>j:
+      if a[i]>=b[j]:
+        myarray.append(b[j])
+        j+=1
+      elif a[i]<=b[j]:
+        myarray.append(a[i])
+        i+=1
+
+    return myarray + a[i:] + b[j:]
+
+a=[1,3,4,6,20]
+b=[2,3,4,5,6,9,11,76]
+x=merged_sorted_array(a,b)
+print(a)
+print(b)
+print(x)
 
